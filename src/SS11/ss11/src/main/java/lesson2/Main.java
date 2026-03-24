90d793d426470dfb95f78d15049ede00811d7698
duydep24022006 <memay26032006@gmail.com>
2026-03-24 09:51:36 +0700

    moi

---
+++ src/SS11/ss11/src/main/java/lesson2/Main.java
@@ -0,0 +1,8 @@
+package lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+        lesson2.MedicineDAO dao = new lesson2.MedicineDAO();
+        dao.printMedicineList();
+    }
+}
